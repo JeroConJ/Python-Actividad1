5d735d5207f242956a924966869668169fbd6ccc
Jeronimo Soto Cilley <jerosotoc@gmail.com>
2025-03-18 00:38:31 -0300

    Modifico el programa y hago el primer punto del punto 7

--- questions.py
+++ questions.py
@@ -5,12 +5,12 @@ questions = ["¿Qué función se usa para obtener la longitud de una cadena en P
 "¿Cómo se solicita entrada del usuario en Python?",
 "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
 "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",]
+
 # Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
 answers = [("size()", "len()", "length()", "count()"),
 ("3.14", "'42'", "10", "True"),
 ("input()", "scan()", "read()", "ask()"),
-(
-"// Esto es un comentario",
+("// Esto es un comentario",
 "/* Esto es un comentario */",
 "-- Esto es un comentario",
 "# Esto es un comentario"),
@@ -18,6 +18,7 @@ answers = [("size()", "len()", "length()", "count()"),
 
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+respuestas_validas = ["1", "2", "3", "4"]
 
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
@@ -31,17 +32,26 @@ for _ in range(3):
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        user_answer = input("Respuesta: ")
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
-            print("¡Correcto!")
-            break
 
+        if user_answer in respuestas_validas:
+            user_answer = int(user_answer) - 1
+            print(user_answer)
+            if user_answer == correct_answers_index[question_index]:
+                print("¡Correcto!")
+                break
+            else:
+                if intento == 0:
+                    print("Incorrecto, trate de nuevo")
         else:
-            # Si el usuario no responde correctamente después de 2 intentos,
-            # se muestra la respuesta correcta
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index][correct_answers_index[question_index]])
+            print("Respuesta no valida")
+            exit(1)
+    else:
+        # Si el usuario no responde correctamente después de 2 intentos,
+        # se muestra la respuesta correcta
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index][correct_answers_index[question_index]])
 
 # Se imprime un blanco al final de la pregunta
 print()
