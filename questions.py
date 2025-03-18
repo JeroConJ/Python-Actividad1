ce5d5a4f0fdb98282745bdd08493abaaadfd1b90
Jeronimo Soto Cilley <jerosotoc@gmail.com>
2025-03-18 19:31:15 -0300

    Modifico el juego para no acceder a las 3 listas usando indices

--- questions.py
+++ questions.py
@@ -20,38 +20,38 @@ answers = [("size()", "len()", "length()", "count()"),
 correct_answers_index = [1, 2, 0, 3, 1]
 respuestas_validas = ["1", "2", "3", "4"]
 puntaje = 0
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
 
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
 
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(f'{questions_to_ask[_][0]}')
+    for i, answer in enumerate(questions_to_ask[_][1]):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = input("Respuesta: ")
-        if user_answer in respuestas_validas:        #Verifica si la respuesta es valida (numero dedl 1 al 4)
+        #Verifica si la respuesta es valida (numero del 1 al 4)
+        if user_answer in respuestas_validas:
             user_answer = int(user_answer) - 1  
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == questions_to_ask[_][2]:
                 print("¡Correcto!")
                 puntaje += 1
                 break
             else:
-                if intento == 0:                    #Si falla una vez, no muestra la respuesta correcta
+                #Si falla una vez, no muestra la respuesta correcta
+                if intento == 0:
                     print("Incorrecto, trate de nuevo")
                     puntaje -= 0.5
         else:
             print("Respuesta no valida")
             exit(1)
     else:
-        # Si el usuario no responde correctamente después de 2 intentos,
-        # se muestra la respuesta correcta
+        # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(questions_to_ask[_][1][questions_to_ask[_][2]])
         puntaje -= 0.5
 
 print(f'Puntaje: {puntaje}')
